aa0fa767be51dab4d13b21fce7a5b38fb2935797
Saurabh Yadav <87260165+Saurabh-314@users.noreply.github.com>
2022-07-21 16:23:58 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,60 +1,62 @@
+// hamburger menu
 const navBtn = document.getElementById('nav-btn');
 const cancelBtn = document.getElementById('cancel-btn');
 const sideNav = document.getElementById('sidenav');
 // const modal = document.getElementById('modal');
 
-navBtn.addEventListener("click", function(){
-    sideNav.classList.add('show');
-    modal.classList.add('showModal');
+navBtn.addEventListener("click", function () {
+  sideNav.classList.add('show');
+  modal.classList.add('showModal');
 });
 
-cancelBtn.addEventListener('click', function(){
+cancelBtn.addEventListener('click', function () {
+  sideNav.classList.remove('show');
+  modal.classList.remove('showModal');
+});
+
+window.addEventListener('click', function (event) {
+  if (event.target === modal) {
     sideNav.classList.remove('show');
     modal.classList.remove('showModal');
+  }
 });
-
-window.addEventListener('click', function(event){
-    if(event.target === modal){
-        sideNav.classList.remove('show');
-        modal.classList.remove('showModal');
-    }
-});
-
-
 
 const submitBtn = document.getElementById("submitBtn");
 
-submitBtn.addEventListener("click", () =>{
-    const indate = document.getElementById("chekin-date").value;
-    const outdate = document.getElementById("chekout-date").value;
-    const adult = document.getElementById("adult").value;
-    const children = document.getElementById("children").value;
-    const rooms = document.getElementById("rooms").value;
+submitBtn.addEventListener("click", () => {
+  const indate = document.getElementById("chekin-date").value;
+  const outdate = document.getElementById("chekout-date").value;
+  const adult = document.getElementById("adult").value;
+  const children = document.getElementById("children").value;
+  const rooms = document.getElementById("rooms").value;
 
-    alert("checkIn Date: "+indate+" check outdate: "+outdate+" adult: "+adult+" children: "+children+" rooms: "+rooms);
+  alert("checkIn Date: " + indate + " check outdate: " + outdate + " adult: " + adult + " children: " + children + " rooms: " + rooms);
 });
 
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementsByClassName("roomBtn");
+
+Array.from(btn).forEach(element => {
+  // When the user clicks the button, open the modal 
+  element.onclick = function () {
+    modal.style.display = "block";
+  }
+})
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
